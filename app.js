4bb4a7029d613d349f76180392794a12113701fd
yujjineee <skekskekskek@gmail.com>
2021-10-15 14:44:12 +0900

    1015 gitignore update

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ const statRouter = require('./routes/stat')
 const connect = require('./mongo')
 connect()
 
+
 const app = express()
 
+
 app.use(express.static('views'))
+
 
 app.set('port', process.env.PORT || 8080)
 app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile)
 
+
 app.use('/', indexRouter)
 app.use('/show', showRouter)
 app.use('/stat', statRouter)
+
 
 app.listen(app.get('port'), () => {
     console.log('PORT', app.get('port'), '번에서 대기중!')
