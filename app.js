5e75c4df095ec99829ff313c5d5d2e4561f8c9b7
yujjineee <skekskekskek@gmail.com>
2021-10-30 00:39:10 +0900

    1030
    json형식으로는 data 출력 가능 / html 렌더링하면서 data 출력 안됨 -> 주말동안 만들기

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const app = express()
 const router = express.Router()
 
 app.use(express.static('views'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.set('port', process.env.PORT || 5000)
 app.set('view engine', 'ejs')
