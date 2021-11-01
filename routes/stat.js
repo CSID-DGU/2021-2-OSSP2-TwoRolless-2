52093d5807a16aea1b33f4caa8f101c1db07a3de
yujjineee <skekskekskek@gmail.com>
2021-11-01 22:04:04 +0900

    1101 세부 페이지 DB 연동
    장소 개별 페이지 DB 연동 완료
    + 카카오맵 추가/정보 Table화/TrueFalse 치환

--- routes/stat.js
+++ routes/stat.js
@@ -5,16 +5,24 @@ const theaters = require('../mongo/theater')
 router.use(express.static('views'))
 
 router.get("/place", (req, res) => {
-   theaters.theaters.find(function (err, theater) {
-        res.render('stat-place.html', {data: theater})
+    theaters.theaters.find(function (err, theater) {
+        try {
+            res.render('stat-place.html', { data: theater })
+        } catch (err) {
+            res.render(Error)
+        }
     })
-    .sort({"CITY" : 1 })
+        .sort({ "TOTAL_SEAT_NUM": -1 })
 })
 
 
 router.get("/place/:id", (req, res) => {
-    theaters.theaters.findOne(req.params.id, (err, theater) => {
-        res.render('place-info.ejs', {data: theater})
+    theaters.theaters.findOne({_id: req.params.id}, (err, theater) => {
+        try {
+            res.render('place-info.ejs', { data: theater })
+        } catch (err) {
+            res.render(Error)
+        }
     })
 })
 
