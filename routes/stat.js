95741d3c7bf85a9a5eca01a2b768f04f2544a2dc
yujjineee <skekskekskek@gmail.com>
2021-12-02 14:13:15 +0900

    1202 공연 트렌드 db 연결 완료
    theater_over, theater_inc 데이터베이스와 stat-trend.html 연결 확인 (백엔드 완료!)

--- routes/stat.js
+++ routes/stat.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const theaters = require('../mongo/theater')
-
-
+const theater_over = require('../mongo/theater_over')
+const theater_inc = require('../mongo/theater_inc')
 
 
 router.use(express.static('views'))
@@ -19,7 +19,7 @@ router.get("/place", (req, res) => {
 })
 
 router.get("/place/:id", (req, res) => {
-    theaters.theaters.findOne({_id: req.params.id}, (err, theater) => {
+    theaters.theaters.findOne({ _id: req.params.id }, (err, theater) => {
         try {
             res.render('place-info.ejs', { data: theater })
         } catch (err) {
@@ -31,13 +31,16 @@ router.get("/place/:id", (req, res) => {
 
 // ====================================
 
-router.get('/trend', (req, res, next) => {
-    try {
-        res.render('stat-trend.html')
-    } catch (err) {
-        console.error(err)
-        next(err)
-    }
+router.get('/trend', async (req, res) => {
+
+    const theater_ov = await theater_over.theater_over.find({})
+    const theater_ic = await theater_inc.theater_inc.find({})
+
+    res.render('stat-trend.html', {
+        data1: theater_ov,
+        data2: theater_ic
+    })
+
 })
 
 module.exports = router
