41269bce1b36872d9c15bc41bb68047b35b533b9
JYOOJIN <ellka02@naver.com>
2021-12-12 03:30:20 +0900

    offline_js

---
+++ mongo/offline_musical.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+const { Types: { ObjectId } } = Schema
+const off_musical = new Schema({
+
+    title: {
+        type: String
+    },
+    place: {
+        type: String
+    },
+    period: {
+        type: String
+    },
+    time: {
+        type: String
+    },
+    age: {
+        type: String
+    },
+    poster: {
+        type: String
+    },
+    price: {
+        type: Array
+    },
+    casting: {
+        type: Array
+    },
+    timeinfo: {
+        type: Array
+    },
+    notice: {
+        type: Array
+    },
+    discount: {
+        type: Array
+    },
+    schedule: {
+        type: Array
+    },
+
+})
+
+module.exports.on_mus = mongoose.model('off_mus', off_musical)
