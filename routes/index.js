a93067fca09933b586bdbe426145572c540f5ffd
yujjineee <skekskekskek@gmail.com>
2021-12-13 22:32:07 +0900

    1213 index 완성

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,24 @@ const sns = require('../mongo/sns')
 
 router.use(express.static('views'))
 
-router.get('/', (req, res, next) => {
-    try {
-        res.render('index.html')
-    } catch (err) {
-        console.error(err)
-        next(err)
-    }
+router.get('/', (req, res) => {
+    sns.sns.find(function (err, sns) {
+        try {
+            res.render('index.ejs', { data: sns })
+        } catch(err) {
+            res.render(err)
+        }
+    }).sort({"time" : 1}).limit(3)
+
 })
+
+
+// try {
+//     res.render('index.html')
+// } catch (err) {
+//     console.error(err)
+//     next(err)
+// }
 
 router.get('/about', (req, res, next) => {
     try {
@@ -37,15 +47,15 @@ router.get('/faq', (req, res, next) => {
 router.get('/SNS', (req, res) => {
     sns.sns.find(function (err, sns) {
         try {
-            res.render('SNS_main.html', {data: sns})
-        } catch(err) {
+            res.render('SNS_main.html', { data: sns })
+        } catch (err) {
             res.render(err)
         }
     })
 })
 
 router.get("/thelastman", (req, res) => {
-    sns.sns.find({ tag:"더라스트맨"}, (err, sns) => {
+    sns.sns.find({ tag: "더라스트맨" }, (err, sns) => {
         try {
             res.render('sns_html/thelastman.html', { data: sns })
         } catch (err) {
@@ -55,7 +65,7 @@ router.get("/thelastman", (req, res) => {
 })
 
 router.get("/thedevil", (req, res) => {
-    sns.sns.find({ tag:"더데빌"}, (err, sns) => {
+    sns.sns.find({ tag: "더데빌" }, (err, sns) => {
         try {
             res.render('sns_html/thedevil.html', { data: sns })
         } catch (err) {
@@ -65,7 +75,7 @@ router.get("/thedevil", (req, res) => {
 })
 
 router.get("/goldmunt", (req, res) => {
-    sns.sns.find({ tag:"나르치스와_골드문트"}, (err, sns) => {
+    sns.sns.find({ tag: "나르치스와_골드문트" }, (err, sns) => {
         try {
             res.render('sns_html/goldmunt.html', { data: sns })
         } catch (err) {
@@ -75,7 +85,7 @@ router.get("/goldmunt", (req, res) => {
 })
 
 router.get("/elphsng", (req, res) => {
-    sns.sns.find({ tag:"엘리펀트송"}, (err, sns) => {
+    sns.sns.find({ tag: "엘리펀트송" }, (err, sns) => {
         try {
             res.render('sns_html/elephantsong.html', { data: sns })
         } catch (err) {
@@ -85,7 +95,7 @@ router.get("/elphsng", (req, res) => {
 })
 
 router.get("/kuroimen", (req, res) => {
-    sns.sns.find({ tag:"쿠로이저택엔누가살고있을까"}, (err, sns) => {
+    sns.sns.find({ tag: "쿠로이저택엔누가살고있을까" }, (err, sns) => {
         try {
             res.render('sns_html/kuroimension.html', { data: sns })
         } catch (err) {
@@ -95,7 +105,7 @@ router.get("/kuroimen", (req, res) => {
 })
 
 router.get("/sshorts", (req, res) => {
-    sns.sns.find({ tag:"난쟁이들"}, (err, sns) => {
+    sns.sns.find({ tag: "난쟁이들" }, (err, sns) => {
         try {
             res.render('sns_html/shorts.html', { data: sns })
         } catch (err) {
@@ -105,7 +115,7 @@ router.get("/sshorts", (req, res) => {
 })
 
 router.get("/gotmr", (req, res) => {
-    sns.sns.find({ tag:"곤투모로우"}, (err, sns) => {
+    sns.sns.find({ tag: "곤투모로우" }, (err, sns) => {
         try {
             res.render('sns_html/gonetomorrow.html', { data: sns })
         } catch (err) {
@@ -115,7 +125,7 @@ router.get("/gotmr", (req, res) => {
 })
 
 router.get("/themmt", (req, res) => {
-    sns.sns.find({ tag:"더모먼트"}, (err, sns) => {
+    sns.sns.find({ tag: "더모먼트" }, (err, sns) => {
         try {
             res.render('sns_html/themoment.html', { data: sns })
         } catch (err) {
@@ -125,7 +135,7 @@ router.get("/themmt", (req, res) => {
 })
 
 router.get("/luckyseven", (req, res) => {
-    sns.sns.find({ tag:"칠칠"}, (err, sns) => {
+    sns.sns.find({ tag: "칠칠" }, (err, sns) => {
         try {
             res.render('sns_html/sevenseven.html', { data: sns })
         } catch (err) {
@@ -135,7 +145,7 @@ router.get("/luckyseven", (req, res) => {
 })
 
 router.get("/fanlttr", (req, res) => {
-    sns.sns.find({ tag:"팬레터"}, (err, sns) => {
+    sns.sns.find({ tag: "팬레터" }, (err, sns) => {
         try {
             res.render('sns_html/fanletter.html', { data: sns })
         } catch (err) {
@@ -145,7 +155,7 @@ router.get("/fanlttr", (req, res) => {
 })
 
 router.get("/kingsman", (req, res) => {
-    sns.sns.find({ tag:"젠틀맨스가이드"}, (err, sns) => {
+    sns.sns.find({ tag: "젠틀맨스가이드" }, (err, sns) => {
         try {
             res.render('sns_html/gentlemansguide.html', { data: sns })
         } catch (err) {
@@ -155,7 +165,7 @@ router.get("/kingsman", (req, res) => {
 })
 
 router.get("/jklhyd", (req, res) => {
-    sns.sns.find({ tag:"지킬앤하이드"}, (err, sns) => {
+    sns.sns.find({ tag: "지킬앤하이드" }, (err, sns) => {
         try {
             res.render('sns_html/jekyllandhyde.html', { data: sns })
         } catch (err) {
@@ -165,7 +175,7 @@ router.get("/jklhyd", (req, res) => {
 })
 
 router.get("/endnote", (req, res) => {
-    sns.sns.find({ tag:"엔딩노트"}, (err, sns) => {
+    sns.sns.find({ tag: "엔딩노트" }, (err, sns) => {
         try {
             res.render('sns_html/endingnote.html', { data: sns })
         } catch (err) {
@@ -175,7 +185,7 @@ router.get("/endnote", (req, res) => {
 })
 
 router.get("/billy", (req, res) => {
-    sns.sns.find({ tag:"빌리"}, (err, sns) => {
+    sns.sns.find({ tag: "빌리" }, (err, sns) => {
         try {
             res.render('sns_html/billyeliet.html', { data: sns })
         } catch (err) {
@@ -185,7 +195,7 @@ router.get("/billy", (req, res) => {
 })
 
 router.get("/silver999", (req, res) => {
-    sns.sns.find({ tag:"은하철도의"}, (err, sns) => {
+    sns.sns.find({ tag: "은하철도의" }, (err, sns) => {
         try {
             res.render('sns_html/999night.html', { data: sns })
         } catch (err) {
@@ -195,7 +205,7 @@ router.get("/silver999", (req, res) => {
 })
 
 router.get("/vmpoth", (req, res) => {
-    sns.sns.find({ tag:"뱀파이어아더"}, (err, sns) => {
+    sns.sns.find({ tag: "뱀파이어아더" }, (err, sns) => {
         try {
             res.render('sns_html/vampireother.html', { data: sns })
         } catch (err) {
