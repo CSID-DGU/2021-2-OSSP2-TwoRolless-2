7bba6d04d334a527e6d788fca70b2a44e7438fa2
yujjineee <skekskekskek@gmail.com>
2021-12-13 12:13:26 +0900

    show

--- routes/index.js
+++ routes/index.js
@@ -32,34 +32,6 @@ router.get('/faq', (req, res, next) => {
     }
 })
 
-
-router.get('/show-musical', (req, res, next) => {
-    try {
-        res.render('show-musical.html',{data: musical})
-    } catch (err) {
-        console.error(err)
-        next(err)
-    }
-})
-
-router.get('/show-play', (req, res, next) => {
-    try {
-        res.render('show-play.html',{data: play})
-    } catch (err) {
-        console.error(err)
-        next(err)
-    }
-})
-
-router.get('/show-concert', (req, res, next) => {
-    try {
-        res.render('show-concert.html',{data: concert})
-    } catch (err) {
-        console.error(err)
-        next(err)
-    }
-})
-
 router.get('/SNS', (req, res) => {
     sns.sns.find(function (err, sns) {
         try {
