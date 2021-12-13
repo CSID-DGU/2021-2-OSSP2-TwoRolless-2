c1b949d5d964f528dede192ede6c6dd581896dcd
yujjineee <skekskekskek@gmail.com>
2021-12-13 17:23:43 +0900

    1213 show-trend 오류 수정 완료

--- routes/stat.js
+++ routes/stat.js
@@ -7,25 +7,6 @@ const theater_inc = require('../mongo/theater_inc')
 router.use(express.static('views'))
 
 router.get("/place", (req, res) => {
-
-    // const seoul = theaters.theaters.find({CITY: "서울"})
-    // const incheon = theaters.theaters.find({CITY: "인천"})
-    // const daejeon = theaters.theaters.find({CITY: "대전"})
-    // const daegu = theaters.theaters.find({CITY: "대구"})
-    // const busan = theaters.theaters.find({CITY: "부산"})
-    // const ulsan = theaters.theaters.find({CITY: "울산"})
-    // const jeju = theaters.theaters.find({CITY: "제주"})
-
-    // const gangwon = theaters.theaters.find({CITY: "강원"})
-    // const gyungbuk = theaters.theaters.find({CITY: "경북"})
-    // const gyungnam = theaters.theaters.find({CITY: "경남"})
-    // const jeonnam = theaters.theaters.find({CITY: "전남"})
-    // const jeonbuk = theaters.theaters.find({CITY: "전북"})
-    // const chungnam = theaters.theaters.find({CITY: "충남"})
-    // const chungbuk = theaters.theaters.find({CITY: "충북"})
-    // const gyunggi = theaters.theaters.find({CITY: "경기"})
-    // const sejong = theaters.theaters.find({CITY: "세종"})
-
     theaters.theaters.find(function (err, theater) {
         try {
             res.render('stat-place.html', { data: theater })
