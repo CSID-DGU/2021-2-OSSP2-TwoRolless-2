892a8fafee4f6f0f77cb48f3cc8dab47d24a76e6
yujjineee <skekskekskek@gmail.com>
2021-12-13 20:31:03 +0900

    1213 place-trend-all 수정 완료

--- routes/stat.js
+++ routes/stat.js
@@ -33,25 +33,29 @@ router.get("/place/:id", (req, res) => {
 router.get('/place-trend', async (req, res) => {
 
     const theater_ic = await theater_inc.theater_inc.find({})
-                                                    .sort({"rank": 1 })
-    try{
+        .sort({ "rank": 1 })
+    try {
         res.render('place-trend.ejs', {
             data: theater_ic
         })
-    } catch(Error) {
+    } catch (Error) {
         res.render(Error)
-    }                                      
-    
+    }
+
 })
 
-router.get('/place-trend-all', (req, res) => {
-    theater_over.theater_over.find(function (err, thea) {
-        try {
-            res.render('place-trend-all.ejs', { data: thea })
-        } catch (err) {
-            res.render(Error)
-        }
-    })
+router.get('/place-trend-all', async (req, res) => {
+
+    try {
+        const seoul = await theater_over.theater_over.find({ ctprvn_nm: "서울특별시" }).sort({ signgu_nm : 1 })
+        const gyunggi = await theater_over.theater_over.find({ ctprvn_nm: "경기도" }).sort({ signgu_nm : 1 })
+        const busan = await theater_over.theater_over.find({ ctprvn_nm: "부산광역시" }).sort({ signgu_nm : 1 })
+        const all = await theater_over.theater_over.find({ ctprvn_nm: { $nin : ["서울특별시", "경기도", "부산광역시"] }}).sort({ ctprvn_nm : 1 })
+
+        res.render('place-trend-all.ejs', { data1: seoul, data2: gyunggi, data3: busan, data4: all })
+    } catch (err) {
+        res.render(Error)
+    }
 })
 
 
